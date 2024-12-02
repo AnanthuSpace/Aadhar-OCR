18ea3709443015b3012abdc4b7db2b82aae694d8
Ananthu Mohan <ananthumohan368@gmail.com>
2024-12-02 16:51:01 +0530

    add images

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.localhostURL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
