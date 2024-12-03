47999cf7d8542ac6891d54439bbe2159d3a040aa
Ananthu Mohan <ananthumohan368@gmail.com>
2024-12-03 11:45:00 +0530

    import correption

--- server/middleware/errorHanler.js
+++ server/middleware/errorHanler.js
@@ -1,10 +1,8 @@
-function notFound(req, res) {
+export const notFound = (req, res) => {
     res.status(404).json({ status: false, msg: "Page not found" })
 }
 
-function errorHandle(err, req, res, next) {
+export const errorHandle = (err, req, res, next)  => {
     console.log(err);
     res.status(500).json({ status: false, msg: "Internal server error" })
 }
-
-module.exports = { notFound, errorHandle }
